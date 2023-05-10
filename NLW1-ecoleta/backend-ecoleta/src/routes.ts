ad623b03b45f90a57846a5a77161880f8192e05e
caionikolas <Caio.kanu@hotmail.com>
2023-05-10 19:53:20 -0300

    relacionamento

--- NLW1-ecoleta/backend-ecoleta/src/routes.ts
+++ NLW1-ecoleta/backend-ecoleta/src/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import knex from './database/connection';
 
 import ItemsController from './controllers/ItemsController'
 
@@ -9,6 +10,45 @@ const routes = express.Router();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
+
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body
+
+    const ids = await knex('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    })
+
+    const point_items = items.map((item_id: number) => {
+        return {
+            item_id, 
+            point_id: ids[0]
+        }
+    })
+
+    await knex('point_items').insert(point_items);
+
+    return response.json({
+        success: true
+    })
+})
+
+
 
 export default routes;
 
