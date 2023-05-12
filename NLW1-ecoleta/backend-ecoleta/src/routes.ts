e28eecd52a9a23c75d99d6db5dfb7bdfc794490d
caionikolas <Caio.kanu@hotmail.com>
2023-05-11 23:36:35 -0300

    backend

--- NLW1-ecoleta/backend-ecoleta/src/routes.ts
+++ NLW1-ecoleta/backend-ecoleta/src/routes.ts
@@ -1,58 +1,21 @@
 import express from 'express';
-import knex from './database/connection';
+
+import PointsController from './controllers/PointsController';
 
 import ItemsController from './controllers/ItemsController'
+
 
 //index , show, create, update, delete
 
 const routes = express.Router();
-
+const pointsController = new PointsController()
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', async (request, response) => {
-    const {
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-        items
-    } = request.body
-
-    const trx = await knex.transaction();
-
-    const insertedIds = await trx('points').insert({
-        image: 'image-fake',
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf
-    })
-
-    const point_id = insertedIds[0]
-
-    const point_items = items.map((item_id: number) => {
-        return {
-            item_id, 
-            point_id
-        }
-    })
-
-    await trx('point_items').insert(point_items);
-
-    return response.json({
-        success: true
-    })
-})
-
-
+routes.post('/points', pointsController.create)
+routes.get('/points', pointsController.index)
+routes.get('/points/:id', pointsController.show)
 
 export default routes;
 
